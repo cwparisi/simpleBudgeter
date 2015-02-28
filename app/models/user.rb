ba54eb7b15de262624b7ab4177f6be9f82e749e5
Chris <ChrisP@glados.gateway.pace.com>
2015-02-28 13:06:18 -0800

    Adds mobile CSS changes, delete children of user if deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-	has_many :transactions
-	has_many :funds
-	has_many :incomes
+	has_many :transactions, :dependent => :destroy
+	has_many :funds, :dependent => :destroy
+	has_many :incomes, :dependent => :destroy
 	validates :userName, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
 
 	def full_name
